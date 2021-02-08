5ea9b55094faa2aa1b6b81f33aa41358409ca882
Magdalena Wojciechowska <magdawoj95@o2.pl>
2021-02-08 12:18:30 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const firstName = "Magdalena";
+const age = 25;
+const name1 = "Andrzej";
+const name2 = "Andżej";
+const name3 = "Andżyj";
+const modul1 = "Witam Cię";
+console.log(firstName);
+console.log(age);
+console.log(`joł, my name is ${firstName} and I'm ${age}yo.`);
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+// console.log(modul1);
+
+console.log(`${name1} ${modul1}`);
+console.log(`${name2} ${modul1}`);
+console.log(`${name3} ${modul1}`);
+alert("dzień dobry");
+
+const a = 1;
+const b = 2;
+
+const wynik = a + b;
+alert(wynik);
